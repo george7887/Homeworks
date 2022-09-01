f9df241a12e5f9a7101698b9734e5021105acf3e
george7887 <george788719@gmail.com>
2022-09-01 18:45:14 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Ведите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > y)
+    Console.WriteLine(x);
+else
+    Console.WriteLine(y);
