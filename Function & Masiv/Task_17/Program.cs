42fc4b183458176f85f33b14f967ab1280ed9749
george7887 <george788719@gmail.com>
2022-09-04 20:16:55 +0700

    Task_17

---
+++ Function & Masiv/Task_17/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter first number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter secend number: ");
+int y = Convert.ToInt32(Console.ReadLine());
+while (x == 0 || y == 0)
+{
+    Console.WriteLine("ValueError!!");
+    x = Convert.ToInt32(Console.ReadLine());
+    y = Convert.ToInt32(Console.ReadLine());
+}
+if (x > 1 && y > 1) 
+    Console.WriteLine("1");
+if (x < 1 && y > 1) 
+    Console.WriteLine("2");
+if (x > 1 && y < 1) 
+    Console.WriteLine("3");
+if (x < 1 && y < 1) 
+    Console.WriteLine("4");
