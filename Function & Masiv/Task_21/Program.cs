3f2f978ca6cf8ed88bba63f5bd2b51ccad44af07
george7887 <george788719@gmail.com>
2022-09-04 20:20:05 +0700

    Task_21

---
+++ Function & Masiv/Task_21/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter x1, y1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter x2, y2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2,2)));
