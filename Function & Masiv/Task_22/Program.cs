b49b9d51f641b2a3b7d400ede1fc1d572129a6ef
george7887 <george788719@gmail.com>
2022-09-04 20:25:28 +0700

    Task_ 22

---
+++ Function & Masiv/Task_22/Program.cs
@@ -0,0 +1,14 @@
+﻿int x, i;
+Console.WriteLine("Enter number: ");
+x = Convert.ToInt32(Console.ReadLine());
+/*int i = 1;
+while (i <= x)
+{
+    int y = i * i;
+    Console.Write(y + " ");
+    i++;
+}*/
+for (i = 1; i <= x; i++)
+{
+    Console.Write(Math.Pow(i, 2) + " ");
+}
