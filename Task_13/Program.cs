3d98bb652a695af2e331588c36256b0625207bef
george7887 <george788719@gmail.com>
2022-09-05 13:40:51 +0700

    Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+while (x / 100 == 0)
+{
+    Console.WriteLine("Попробуйте еще раз: ");
+    x = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(x % 1000 / 100);
