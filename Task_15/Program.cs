d4b86984519416a45fe24fb682fa538d483361f4
george7887 <george788719@gmail.com>
2022-09-05 13:32:33 +0700

    Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите номер дня недели: ");
+int x = Convert.ToInt32(Console.ReadLine());
+while (x < 1 || x >7)
+{
+    Console.Write("Нет такого дня недели, попробуйте еще: ");
+    x = Convert.ToInt32(Console.ReadLine());
+}
+
+if (x == 6 || x == 7)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
