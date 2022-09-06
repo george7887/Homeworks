0f4f88dbe09f471976c4ba23120751aaa4ddcaf4
george7887 <george788719@gmail.com>
2022-09-06 21:17:11 +0700

    Task_21

---
+++ Task_21/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите x1, y1, z1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x2, y2, z2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)));
