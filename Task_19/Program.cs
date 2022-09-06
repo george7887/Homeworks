acb37d1bc2c499dedae206b4ef690d0635a56454
george7887 <george788719@gmail.com>
2022-09-06 21:08:16 +0700

    Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+while (x / 10000 == 0 || x / 100000 != 0)
+{
+    Console.WriteLine("Не верноб попробуйте еще раз: ");
+    x = Convert.ToInt32(Console.ReadLine());
+}
+
+if (x / 10000 == x % 10 & x / 1000 % 10 == x % 100 / 10)
+    Console.WriteLine("да"); 
+else
+    Console.WriteLine("нет");
