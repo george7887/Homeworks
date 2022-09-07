0b3ae21f7fa3d4a05779341bbac886ed30cc8d72
george7887 <george788719@gmail.com>
2022-09-07 14:50:51 +0700

    Исправил задание

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,5 +1,11 @@
-﻿Console.WriteLine("Введите число: ");
+﻿int t = 1, N = 0, y = 10;
+Console.WriteLine("Введите число: ");
 int x = Convert.ToInt32(Console.ReadLine());
-if (x / 100 == 0) Console.WriteLine("Третьей цыфры нет!");
+for (int i = 10;t != 0; i = i * 10)
+{
+    t = x / i;
+    N++;
+}
+if (x / 100 == 0) Console.WriteLine("Третьей цифры нет!");
 else
-    Console.WriteLine(x % 1000 / 100);
+    Console.WriteLine(x / Convert.ToInt32(Math.Pow(y, N-3)) % y);
