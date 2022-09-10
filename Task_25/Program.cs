f06d708e4d2bd8762ed9d13428bb2391227c2e00
george7887 <george788719@gmail.com>
2022-09-10 22:50:06 +0700

    Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите значение A: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение B: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int pow = 1;
+for (int i = 1; i <= y; i++)
+{
+    pow = pow * x;
+}
+
+Console.WriteLine(pow);
